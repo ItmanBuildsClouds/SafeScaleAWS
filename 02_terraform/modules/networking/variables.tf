674e50419b056163e0c1421cb476b409f8ff7c68
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-16 16:56:01 +0200

    feat(networking): Build VPC with public/private subnets,routing and security groups

---
+++ 02_terraform/modules/networking/variables.tf
@@ -0,0 +1,27 @@
+variable "cidr_block" {
+    description = "CIDR block for the VPC"
+    type = string
+    default = "10.0.0.0/16"
+}
+variable "project_name" {
+    type = string
+    default = "safescale-aws"
+}
+variable "environment" {
+    type = string
+    default = "dev"
+}
+variable "public_cidrs" {
+    type = map(string)
+    default = {
+        "a" = "10.0.1.0/24"
+        "b" = "10.0.2.0/24"
+    }
+}
+variable "private_app_cidrs" {
+    type = map(string)
+    default = {
+        "a" = "10.0.3.0/24"
+        "b" = "10.0.4.0/24"
+    }
+}
